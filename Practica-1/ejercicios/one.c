63518d75383b2098a3d9b07c1949ce18cd9ff19e
José Luis de la Hoz <joseluisdelahozgarcia@gmail.com>
2019-11-15 17:48:14 +0100

    Practica 1: ejercicio 1

---
+++ Practica-1/ejercicios/one.c
@@ -0,0 +1,23 @@
+#include <mpi.h>
+#include <stdio.h>
+
+
+int main(int argc, char** argv){
+    int nproc, myrank;
+    double start, finish, time;
+
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &nproc);
+    MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
+
+    MPI_Barrier(MPI_COMM_WORLD);
+    start = MPI_Wtime();
+    printf("Hola mundo, soy el proceso %d de un total %d.\n", myrank, nproc);
+    MPI_Barrier(MPI_COMM_WORLD);
+    finish = MPI_Wtime();
+    time = finish - start;
+    printf("Total time of %d: %f\n", myrank, time);
+
+    MPI_Finalize();
+    return 0;
+}
