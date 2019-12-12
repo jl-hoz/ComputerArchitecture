641f4ad5240b867f0f2fa8db5844f7ab1b44890d
Alonso Martín-Toledano <37222828+AlonsoMartinToledano@users.noreply.github.com>
2019-12-12 10:14:09 +0100

    Update one.c

--- Practica-1/ejercicios/one.c
+++ Practica-1/ejercicios/one.c
@@ -5,6 +5,9 @@
 int main(int argc, char** argv){
     int nproc, myrank;
     double start, finish, time;
+    FILE *fp;
+
+    fp = fopen("./test.txt","a");
 
     MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD, &nproc);
@@ -18,6 +21,15 @@ int main(int argc, char** argv){
     time = finish - start;
     printf("Total time of %d: %f\n", myrank, time);
 
+    if(fp == NULL){
+        printf("Error!");               
+    }
+    else{
+        fprintf(fp,"%lf;",time);
+        fclose(fp);
+    }
+
     MPI_Finalize();
+
     return 0;
 }
