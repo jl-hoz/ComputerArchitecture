bca74a33cba7932ba7a33607e47a8099a30972d3
Alonso Martín-Toledano <37222828+AlonsoMartinToledano@users.noreply.github.com>
2019-12-13 15:50:01 +0100

    Update two.cpp

--- Practica-1/ejercicios/two.cpp
+++ Practica-1/ejercicios/two.cpp
@@ -3,18 +3,22 @@
 
 int main(int argc, char **argv){
   int rank, nproc;
+  int read, data;
+  double start, finish, time;
+
   MPI_Status status;
   MPI_Init(&argc, &argv);
   MPI_Comm_size(MPI_COMM_WORLD, &nproc);
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-  int read, data;
-
+  MPI_Barrier(MPI_COMM_WORLD);
+  start = MPI_Wtime();
   // Father send data
   if (rank == 0){
     std::cout << "Introduzca un numero entero: ";
     std::cin >> read;
     printf("\nParent process with rank %d send %d\n", rank, read);
+
     MPI_Send(&read, 1, MPI_INT, 1, 100, MPI_COMM_WORLD);
   }
   // Son receive data and send it to another son.
@@ -22,6 +26,11 @@ int main(int argc, char **argv){
     MPI_Recv(&data, 1, MPI_INT, (rank - 1), 100, MPI_COMM_WORLD, &status);
     printf("\nProcess with rank %d receive %d\n", rank, data);
     data++;
+    if (rank == nproc - 1){
+      finish = MPI_Wtime();
+      time = finish - start;
+      printf("\nTotal time of %d process: %f\n", nproc, time);
+    }
     // Last son do not send data
     if (rank < nproc - 1){
       MPI_Send(&data, 1, MPI_INT, (rank + 1), 100, MPI_COMM_WORLD);
